c39ec64469e80fe9ea19a7fba4b2dcd17522edd0
Kiet Pham <kietpham.dev@gmail.com>
2025-08-30 17:13:09 +1000

    Extracting chat Service

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,16 +1,11 @@
 import type { Request, Response } from 'express';
 
-import OpenAI from 'openai';
-import { conversationRepository } from './repositories/conversation.repository';
+import { chatService } from './services/chat.service';
 import dotenv from 'dotenv';
 import express from 'express';
 import z from 'zod';
 
 dotenv.config();
-
-const client = new OpenAI({
-  apiKey: process.env.OPEN_API_KEY,
-});
 
 const app = express();
 app.use(express.json());
@@ -35,19 +30,9 @@ app.post('/api/chat', async (req: Request, res: Response) => {
   try {
     const { prompt, conversationId } = req.body;
 
-    // models: https://platform.openai.com/docs/models/compare?model=o4-mini
-    const response = await client.responses.create({
-      model: 'gpt-4o-mini',
-      input: prompt,
-      temperature: 0.2,
-      max_output_tokens: 100,
-      previous_response_id:
-        conversationRepository.getLastResponseId(conversationId),
-    });
+    const response = await chatService.sendMessage(prompt, conversationId);
 
-    conversationRepository.setLastResponseId(conversationId, response.id);
-
-    res.json({ message: response.output_text });
+    res.json({ message: response.message });
   } catch (error) {
     res.status(500).json({ error: 'Failed to generate a response.' });
   }
---
+++ packages/server/services/chat.service.ts
@@ -0,0 +1,38 @@
+import OpenAI from 'openai';
+import { conversationRepository } from '../repositories/conversation.repository';
+
+const client = new OpenAI({
+  apiKey: process.env.OPEN_API_KEY,
+});
+
+type ChatResponse = {
+  id: string;
+  message: string;
+};
+
+async function sendMessage(
+  prompt: string,
+  conversationId: string
+): Promise<ChatResponse> {
+  const response = await client.responses.create({
+    // models: https://platform.openai.com/docs/models/compare?model=o4-mini
+    model: 'gpt-4o-mini',
+    input: prompt,
+    temperature: 0.2,
+    max_output_tokens: 100,
+    previous_response_id:
+      conversationRepository.getLastResponseId(conversationId),
+  });
+
+  conversationRepository.setLastResponseId(conversationId, response.id);
+
+  return {
+    id: response.id,
+    message: response.output_text,
+  };
+}
+
+// public interface
+export const chatService = {
+  sendMessage,
+};
