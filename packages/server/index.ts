3dd8834f29f735258a604e63d0a282abdf4768c9
Kiet Pham <kietpham.dev@gmail.com>
2025-08-30 14:10:51 +1000

    Building the chat api

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,11 +1,17 @@
 import type { Request, Response } from 'express';
 
+import OpenAI from 'openai';
 import dotenv from 'dotenv';
 import express from 'express';
 
 dotenv.config();
 
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
@@ -14,6 +20,20 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/api/hello', (req: Request, res: Response) => {
   res.json({ message: 'Hello World' });
+});
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+  const { prompt } = req.body;
+
+  // models: https://platform.openai.com/docs/models/compare?model=o4-mini
+  const response = await client.responses.create({
+    model: 'gpt-4o-mini',
+    input: prompt,
+    temperature: 0.2,
+    max_output_tokens: 100,
+  });
+
+  res.json({ message: response.output_text });
 });
 
 app.listen(port, () => {
