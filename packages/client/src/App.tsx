a77c41b97afb9500e5ba28dcf412deac9c2fe628
Kiet Pham <kietpham.dev@gmail.com>
2025-08-30 23:16:58 +1000

    Handling Form Submition

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -4,7 +4,7 @@ import ChatBot from './components/ChatBot';
 
 function App() {
   return (
-    <div className="">
+    <div className="p-4 h-screen w-full">
       <ChatBot />
     </div>
   );
--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -1,17 +1,51 @@
 import { Button } from './ui/button';
 import { FaArrowUp } from 'react-icons/fa';
+import type { KeyboardEvent } from 'react';
+import { useForm } from 'react-hook-form';
+
+type ChatFormData = {
+  prompt: string;
+};
+
 const ChatBot = () => {
+  const { register, handleSubmit, reset, formState } = useForm<ChatFormData>();
+
+  const submit = (data: ChatFormData) => {
+    console.log(data);
+    reset({ prompt: '' });
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLFormElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      handleSubmit(submit)();
+    }
+  };
+
   return (
-    <div className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl">
+    <form
+      onSubmit={handleSubmit(submit)}
+      onKeyDown={handleKeyDown}
+      className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+    >
       <textarea
+        {...register('prompt', {
+          required: true,
+          validate: (data) => data.trim().length > 0,
+        })}
         placeholder="Ask anything"
         className="w-full border-0 focus:outline-0 resize-none"
+        autoFocus
         maxLength={1000}
       />
-      <Button className="rounded-full w-9 h-9">
+      <Button
+        disabled={!formState.isValid}
+        className="rounded-full w-9 h-9"
+        type="submit"
+      >
         <FaArrowUp />
       </Button>
-    </div>
+    </form>
   );
 };
 
